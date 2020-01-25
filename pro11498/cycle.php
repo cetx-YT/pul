cba364826309d6ef0a28a08d158c8b51d3e1051e
ilya773 <60279256+ilya773@users.noreply.github.com>
2020-01-25 09:30:15 +0700

    синтаксис и циклы

---
+++ pro11498/cycle.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+for ($i = 1; $i<=100; $i++){
+    echo $i."\n";
+}
+
+
+
+for ($i = 23; $i<=78; $i++){
+    echo $i."\n";
+}
+
+
+
+for ($i = 1; $i<=10; $i++){
+    echo "-","<br>";
+}
+
+
+
+for ($i = 1; $i<=100; $i++){
+    echo rand(0, 100)."\n";
+}
+
+
+
+$i = 1;
+while($i<=100){
+    echo rand(0, 100)."\n";
+    $i++;
+}
+
+
+
+
+$arr = array_fill(0, 100, null);
+foreach ($arr as $value)
+{
+    $value = rand(1, 100);
+    echo $value."\n";
+}
+
+
+"<style>
+div.test {
+background: aqua;
+width: 100px;
+height: 100px;
+}
+ </style>
+<div class='test'>
+</div>";
+*/
+
+for ($i = 0; $i<=50; $i++)
+{
+    echo rand(0, 100)."\n";
+}
---
+++ pro11498/index.php
@@ -0,0 +1,61 @@
+<?php
+/*
+$number = 88888888;
+$int = 5;
+echo "В данном числе:"."<br />".substr_count($number, $int);
+
+
+
+$htmllab = "ru";
+echo $htmllab."<br>";
+
+
+
+$NPP = 10;
+echo $NPP;
+
+
+
+$num1= 23;
+$num2 = 67;
+echo $num1+$num2;
+
+
+
+$test = "345";
+echo gettype($test);
+
+
+
+echo "103"+2;
+
+
+*/
+$dayOfWeek = 6;
+switch ($dayOfWeek) {
+    case 1:
+        echo "Сегодня понедельник"."<br>";
+        break;
+    case 2:
+        echo "Сегодня вторник"."<br>";
+        break;
+    case 3:
+        echo "Сегодня среда"."<br>";
+        break;
+    case 4:
+        echo "Сегодня четверг"."<br>";
+        break;
+    case 5:
+        echo "Сегодня пятница"."<br>";
+        break;
+        echo "Сегодня суббота"."<br>";
+        break;
+    case 7:
+        echo "Сегодня воскресенье"."<br>";
+      break;
+   default:
+        echo "Такого дня недели нет"."<br>";
+}
+
+
+
